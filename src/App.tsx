a06486bc532293e6bd72b27e319901856950342e
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-02-09 20:27:08 +0500

    feat(style): replace components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Button from "./components/ui/Button/Button.tsx";
 import Input from "./components/ui/Input/Input.tsx";
+import Card from "./components/ui/Card/Card.tsx";
 
 function App() {
   return (
     <div className='flex bg-slate-300 items-center justify-center w-full h-screen'>
-      <div className='p-4 rounded-md bg-white'>
+      <Card>
         <h1 className='font-bold mb-10 text-center text-2xl'>Welcome back</h1>
 
         <form className='flex flex-col gap-2'>
@@ -29,7 +30,7 @@ function App() {
             palette='secondary'
           />
         </form>
-      </div>
+      </Card>
     </div>
   )
 }
