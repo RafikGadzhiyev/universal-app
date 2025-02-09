549faf3e6a433ed431c4e995763f64cf637cddba
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-02-09 20:15:47 +0500

    feat: replace components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import Button from "./components/ui/Button/Button.tsx";
+import Input from "./components/ui/Input/Input.tsx";
 
 function App() {
   return (
-    <div className='flex bg-secondary-500 items-center justify-center w-full h-screen'>
-      <div className='p-4 rounded-md bg-primary-500 text-white'>
+    <div className='flex bg-slate-300 items-center justify-center w-full h-screen'>
+      <div className='p-4 rounded-md bg-white'>
         <h1 className='font-bold mb-10 text-center text-2xl'>Welcome back</h1>
 
         <form className='flex flex-col gap-2'>
-          <input
+          <Input
+            label="Email"
             placeholder="Email"
             type="email"
             aria-label="Email input"
-            className='placeholder-white rounded-sm px-2 py-1 border outline-none'
             required
           />
 
-          <input
-            placeholder="Password"
+          <Input
+            label="Password"
             type="password"
             aria-label="Password input"
-            className='placeholder-white rounded-sm px-2 py-1 border outline-none'
+            placeholder="Password"
             required
           />
 
