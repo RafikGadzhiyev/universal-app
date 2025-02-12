76782a5b2eaf0176bcd934bc9237c8cedf1e622f
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-02-12 23:41:14 +0500

    feat: add forms to the index page for tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
-import Button from "./components/ui/Button/Button.tsx";
-import Input from "./components/ui/Input/Input.tsx";
-import Card from "./components/ui/Card/Card.tsx";
+import SignInForm from "./components/SignInForm.tsx";
+import SignUpForm from "./components/SignUpForm.tsx";
 
 function App() {
   return (
     <div className='flex bg-slate-300 items-center justify-center w-full h-screen'>
-      <Card>
-        <h1 className='font-bold mb-10 text-center text-2xl'>Welcome back</h1>
-
-        <form className='flex flex-col gap-2'>
-          <Input
-            label="Email"
-            placeholder="Email"
-            type="email"
-            aria-label="Email input"
-            required
-          />
-
-          <Input
-            label="Password"
-            type="password"
-            aria-label="Password input"
-            placeholder="Password"
-            required
-          />
-
-          <Button
-            label="Log In"
-            palette='secondary'
-          />
-        </form>
-      </Card>
+      <SignInForm/>
+      {/*<SignUpForm/>*/}
     </div>
   )
 }
