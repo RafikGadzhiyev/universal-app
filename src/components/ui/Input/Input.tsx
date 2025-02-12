761e76fb6cde205eefccdfa079b13932424d2ad4
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-02-12 23:37:38 +0500

    feat(style): add hint and error text to Input component

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -1,4 +1,6 @@
-import { InputHTMLAttributes, PropsWithChildren } from "react";
+// TODO: during migration to React 19 => Do we need react hook form ?
+
+import { forwardRef, ForwardedRef, InputHTMLAttributes, PropsWithChildren} from "react";
 import { VariantProps } from "class-variance-authority";
 
 import { inputVariants } from "./Input.variants.ts";
@@ -9,48 +11,70 @@ import './Input.style.css'
 interface IInputProps extends PropsWithChildren, InputHTMLAttributes<HTMLInputElement>, VariantProps<typeof inputVariants> {
   label?: string,
   dense?: boolean,
+  error?: string,
+  hint?: string
 }
 
-function Input(props: IInputProps){
-  const {
-    label,
-    palette,
-    variant,
-    className,
-    dense,
-    ...tagProps
-  } = props;
-
-  const classesFromVariants = inputVariants(
-    {
+const Input = forwardRef(
+  function Input(props: IInputProps, ref: ForwardedRef<HTMLInputElement>){
+    const {
+      label,
       palette,
       variant,
-    }
-  )
-
-  const classesToMerge = collectMergeableClasses(
-    [
-      classesFromVariants,
       className,
-      dense ? 'cp-input__dense' : 'cp-input__sparse',
-    ]
-  )
+      dense,
+      error,
+      hint,
+      ...tagProps
+    } = props;
 
-  const mergedClasses = mergeClasses(classesToMerge)
+    const classesFromVariants = inputVariants(
+      {
+        palette: error ? 'negative' : palette,
+        variant,
+      }
+    )
 
-  return (
-    <div className='flex flex-col'>
-      <label className="text-black text-xs">
-        { label }
-      </label>
+    const classesToMerge = collectMergeableClasses(
+      [
+        classesFromVariants,
+        className,
+        dense ? 'cp-input__dense' : 'cp-input__sparse',
+      ]
+    )
 
-      <input
-        type="text"
-        {...tagProps}
-        className={mergedClasses}
-      />
-    </div>
-  )
-}
+    const mergedClasses = mergeClasses(classesToMerge)
+
+    return (
+      <div className='flex flex-col'>
+        <label className="text-black text-xs">
+          { label }
+        </label>
+
+        <input
+          ref={ref}
+          type="text"
+          {...tagProps}
+          className={mergedClasses}
+        />
+
+        {
+          !!error &&
+          <span className='text-negative-500 text-xs'>
+            { error }
+          </span>
+        }
+
+        {
+          !error &&
+          !!hint &&
+          <span className='text-gray-400 text-xs'>
+            { hint }
+          </span>
+        }
+      </div>
+    )
+  }
+)
 
 export default Input;
